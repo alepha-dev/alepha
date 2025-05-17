4eaad1777d0145eb49621a83a14f18a406f57c94
Nicolas <46966636+feunard@users.noreply.github.com>
2025-05-17 13:18:56 +0200

    react: add useApi<T>

--- packages/react/src/hooks/useClient.ts
+++ packages/react/src/hooks/useClient.ts
@@ -4,3 +4,7 @@ import { useInject } from "./useInject.ts";
 export const useClient = (): HttpClient => {
 	return useInject(HttpClient);
 };
+
+export const useApi = <T extends object>() => {
+	return useInject(HttpClient).of<T>();
+};
