e7678ad608f7f8b88496440a64f9ff58d8865e49
Nicolas <46966636+feunard@users.noreply.github.com>
2025-05-17 12:59:01 +0200

    alepha: improve typings auto-complete

--- scripts/build.ts
+++ scripts/build.ts
@@ -1,4 +1,7 @@
 import { $command } from "@alepha/cli";
+import { join } from "node:path";
+import { copyFile, readdir, writeFile } from "node:fs/promises";
+import { existsSync } from "node:fs";
 
 export const build = $command({
 	when: ["build"],
@@ -9,6 +12,23 @@ export const build = $command({
 		await run(
 			`yarn workspaces foreach -Apt run build`,
 		);
+
+		await improveTypingsIndex();
 	},
 })
 
+async function improveTypingsIndex() {
+	const root = join(process.cwd(), "packages");
+	const packages = await readdir(root);
+	for (const name of packages) {
+		const dist = join(root, name, "dist/index.d.ts");
+		const alephaIndex =
+			name.includes("-")
+				? join(root, "alepha", `${name.replace("-", "/")}.d.ts`)
+				: join(root, "alepha", `${name}.d.ts`);
+
+		if (existsSync(dist) && existsSync(alephaIndex)) {
+			await copyFile(dist, alephaIndex);
+		}
+	}
+}
