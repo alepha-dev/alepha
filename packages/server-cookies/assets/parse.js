6d67ff9fc1d9d663f508d9196a92f0900e305e3e
Nicolas <46966636+feunard@users.noreply.github.com>
2025-06-30 22:58:22 +0200

    project: remove useless file

--- packages/server-cookies/assets/parse.js
+++
@@ -1,8 +0,0 @@
-import { inflateRawSync } from "node:zlib";
-
-const tokens = ""
-
-const {access_token} = JSON.parse(inflateRawSync(Buffer.from(tokens, "base64"), { to: "string" }))
-const payload = JSON.parse(Buffer.from(access_token.split(".")[1], "base64").toString())
-
-console.log(payload)
