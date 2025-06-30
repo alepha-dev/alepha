357b45960f7e8dd78fa17831944a6c3df34293aa
Nicolas <46966636+feunard@users.noreply.github.com>
2025-06-30 23:11:08 +0200

    cache: fix tests

--- packages/cache/test/$cache.spec.ts
+++ packages/cache/test/$cache.spec.ts
@@ -98,5 +98,5 @@ test("$cache - unique key", async () => {
 
 	// [] means no args, it's JSON.stringify([])
 	const obj = await app.get(MemoryCacheProvider).get("A:task", "[]");
-	expect(new TextDecoder().decode(obj?.slice(1))).toEqual('"DONE"');
+	expect(new TextDecoder().decode(obj?.slice(1))).toEqual('DONE');
 });
