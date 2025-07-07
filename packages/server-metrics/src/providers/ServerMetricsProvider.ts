1bdfb63a89154f0ea9aff62ba43e06afac7211f6
Nicolas <ni.foures@gmail.com>
2025-07-07 15:36:43 +0200

    server-metrics: collect on start

--- packages/server-metrics/src/providers/ServerMetricsProvider.ts
+++ packages/server-metrics/src/providers/ServerMetricsProvider.ts
@@ -1,11 +1,18 @@
-import { $inject, Alepha } from "@alepha/core";
-import type { ActionDescriptor } from "@alepha/server";
-import { $action } from "@alepha/server";
-import { collectDefaultMetrics, Registry } from "prom-client";
+import {$hook, $inject, Alepha, HookDescriptor} from "@alepha/core";
+import type {ActionDescriptor} from "@alepha/server";
+import {$action} from "@alepha/server";
+import {collectDefaultMetrics, Registry} from "prom-client";
 
 export class ServerMetricsProvider {
-	protected readonly register: Registry;
+	protected readonly register: Registry = new Registry();
 	protected readonly alepha: Alepha = $inject(Alepha);
+
+	public options: {
+		prefix?: string;
+		gcDurationBuckets?: number[];
+		eventLoopMonitoringPrecision?: number;
+		labels?: object;
+	} = {}
 
 	public readonly metrics: ActionDescriptor = $action({
 		method: "GET",
@@ -16,11 +23,13 @@ export class ServerMetricsProvider {
 		handler: () => this.register.metrics(),
 	});
 
-	constructor() {
-		this.register = new Registry();
-		collectDefaultMetrics({
-			register: this.register,
-			prefix: `${(this.alepha.env.APP_NAME ?? "app").toLowerCase()}_`,
-		});
-	}
+	protected readonly onStart: HookDescriptor<"start"> = $hook({
+		name: "start",
+		handler: () => {
+			collectDefaultMetrics({
+				register: this.register,
+				...this.options
+			});
+		}
+	})
 }
