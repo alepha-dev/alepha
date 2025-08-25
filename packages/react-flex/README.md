d8cf0e3dfc8c255288097e1c72f140c9c84a7907
Nicolas <46966636+feunard@users.noreply.github.com>
2025-08-25 10:32:05 +0200

    project: remove react flex

--- packages/react-flex/README.md
+++
@@ -1,1 +0,0 @@
-# Alepha React Flex
--- packages/react-flex/src/index.ts
+++
@@ -1,3 +0,0 @@
-export { type Breakpoint, default as Flex } from "./Flex.tsx";
-export { default as Grid } from "./Grid.tsx";
-export { default as Text } from "./Text.tsx";
