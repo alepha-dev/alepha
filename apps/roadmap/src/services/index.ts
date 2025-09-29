99f4a246020afcfeae35a325f437c823e7442a97
Nicolas Foures <ni.foures@gmail.com>
2025-09-29 22:47:57 +0200

    roadmap: move project to standalone repo

--- apps/roadmap/src/services/index.ts
+++
@@ -1,9 +0,0 @@
-import { $module } from "@alepha/core";
-import { CharacterInfo } from "./CharacterInfo.ts";
-import { I18n } from "./I18n.ts";
-import { Toaster } from "./Toaster.ts";
-
-export const RoadmapServices = $module({
-	name: "roadmap.services",
-	services: [Toaster, I18n, CharacterInfo],
-});
