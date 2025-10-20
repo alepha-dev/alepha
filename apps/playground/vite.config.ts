c597a20271e3a50be9cb5188b82a1cdc642e650a
Nicolas Foures <ni.foures@gmail.com>
2025-10-21 01:16:04 +0200

    project: upgrade deps

--- apps/playground/vite.config.ts
+++ apps/playground/vite.config.ts
@@ -1,12 +1,10 @@
 import { viteAlepha } from "@alepha/vite";
-import viteTailwind from "@tailwindcss/vite";
 import viteReact from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
 export default defineConfig({
   plugins: [
     viteReact(),
-    viteTailwind(),
     viteAlepha({
       serverEntry: "src/index.server.ts",
     }),
