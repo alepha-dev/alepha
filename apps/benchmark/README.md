d14a5dd26a1b50c6c4b378dab29c520708591ed8
Nicolas Foures <ni.foures@gmail.com>
2025-11-18 20:14:03 +0100

    project: move benchmark outside

--- apps/benchmark/README.md
+++
@@ -1,7 +0,0 @@
-Disclaimer: This is not a serious benchmark, just a fun way to compare different frameworks in a simple way.
-
-## Commands
-
-```bash
-yarn benchmark
-```
--- apps/benchmark/src/3002-fastify.ts
+++
@@ -1,9 +0,0 @@
-import Fastify from "fastify";
-
-const app = Fastify();
-
-app.get("/ping", async () => "pong");
-
-app.listen({ port: 3002 }).then(() => {
-  console.log("Fastify listening on :3002");
-});
