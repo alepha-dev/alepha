a926dd437e6acdeff9bec4e66522ee851d0b4fdd
Nicolas Foures <ni.foures@gmail.com>
2025-12-04 16:14:55 +0100

    feat(cli): add command extension via alepha.config.ts

--- packages/alepha/src/cli/apps/AlephaCli.ts
+++ packages/alepha/src/cli/apps/AlephaCli.ts
@@ -1,4 +1,6 @@
-import { $module } from "alepha";
+import { join } from "node:path";
+import { $hook, $inject, $module, Alepha } from "alepha";
+import { FileSystemProvider } from "alepha/file";
 import { BiomeCommands } from "../commands/BiomeCommands.ts";
 import { CoreCommands } from "../commands/CoreCommands.ts";
 import { DrizzleCommands } from "../commands/DrizzleCommands.ts";
@@ -6,9 +8,33 @@ import { VerifyCommands } from "../commands/VerifyCommands.ts";
 import { ViteCommands } from "../commands/ViteCommands.ts";
 import { ProcessRunner } from "../services/ProcessRunner.ts";
 
+class AlephaCliExtension {
+  protected readonly alepha = $inject(Alepha);
+  protected readonly fs = $inject(FileSystemProvider);
+  protected readonly onConfigure = $hook({
+    on: "configure",
+    handler: async () => {
+      const extensionPath = join(process.cwd(), "alepha.config.ts");
+      const hasExtension = await this.fs.exists(extensionPath);
+      if (!hasExtension) {
+        return;
+      }
+
+      // import
+      const { default: Extension } = await import(extensionPath);
+      if (typeof Extension !== "function") {
+        return;
+      }
+
+      this.alepha.with(Extension);
+    },
+  });
+}
+
 export const AlephaCli = $module({
   name: "alepha.cli",
   services: [
+    AlephaCliExtension,
     ProcessRunner,
     CoreCommands,
     DrizzleCommands,
