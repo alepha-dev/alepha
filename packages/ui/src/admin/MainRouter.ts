e4e9eb3283eb66e342bef0f7d995d9b4e02b1cac
Nicolas Foures <ni.foures@gmail.com>
2025-12-04 18:52:30 +0100

    ui/admin: remove css import inside ts (not working with tsdown build)

--- packages/ui/src/admin/MainRouter.ts
+++ packages/ui/src/admin/MainRouter.ts
@@ -1,9 +1,7 @@
 import { $page } from "@alepha/react";
-import { $head } from "@alepha/react/head";
 import { AlephaMantineProvider } from "@alepha/ui";
 import { AuthRouter } from "@alepha/ui/auth";
 import { $inject } from "alepha";
-import href from "../../styles.css?url";
 import { AdminRouter } from "./AdminRouter.ts";
 
 /**
@@ -17,10 +15,6 @@ import { AdminRouter } from "./AdminRouter.ts";
 export class MainRouter {
   auth = $inject(AuthRouter);
   admin = $inject(AdminRouter);
-
-  styles = $head(() => ({
-    link: [{ rel: "stylesheet", href }],
-  }));
 
   layout = $page({
     component: AlephaMantineProvider,
