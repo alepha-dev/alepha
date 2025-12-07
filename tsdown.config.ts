1b1fa4c21553a6b7a2632d14137b375b457b671c
Nicolas Foures <ni.foures@gmail.com>
2025-12-07 23:28:01 +0100

    project: fix tsdown build

--- tsdown.config.ts
+++ tsdown.config.ts
@@ -1,36 +1,41 @@
-import { readFile } from "node:fs/promises";
+import { access } from "node:fs/promises";
+import { join } from "node:path";
 
 export default async () => {
-  let hasBrowser = false;
-  try {
-    await readFile("index.browser.ts");
-    hasBrowser = true;
-  } catch {}
+  const root = process.cwd();
+  const hasBrowser = await access(join(root, "src/index.browser.ts"))
+    .then(() => true)
+    .catch(() => false);
 
-  // check if "index.browser.ts" exists in dir
-	if (hasBrowser) {
-		return [
-			{
-				entry: "./src/index.ts",
-				format: ["esm"],
-				sourcemap: true,
+  if (hasBrowser) {
+    return [
+      {
+        entry: join(root, "src/index.ts"),
+        format: ["esm"],
+        sourcemap: true,
         fixedExtension: false,
+        outDir: join(root, "dist"),
+        dts: true,
       },
-			{
-				entry: "./src/index.browser.ts",
-				platform: "browser",
-				sourcemap: true,
-				dts: false,
-			},
-		];
-	}
-	return [
-		{
-			entry: "./src/index.ts",
-			format: ["esm"],
-			platform: "neutral", // TODO: index.node.ts for node specific build
-			sourcemap: true,
+      {
+        entry: join(root, "src/index.browser.ts"),
+        platform: "browser",
+        sourcemap: true,
+        dts: false,
+        outDir: join(root, "dist"),
+      },
+    ];
+  }
+
+  return [
+    {
+      entry: join(root, "src/index.ts"),
+      format: ["esm"],
+      platform: "neutral", // TODO: index.node.ts for node specific build
+      sourcemap: true,
       fixedExtension: false,
-		},
-	];
-}
+      outDir: join(root, "dist"),
+      dts: true,
+    },
+  ];
+};
