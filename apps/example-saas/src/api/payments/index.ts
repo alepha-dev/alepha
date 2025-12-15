610ba5d399cd33cfa76704b3ff6923f0d8d3f6c8
Nicolas Foures <ni.foures@gmail.com>
2025-12-15 19:08:22 +0100

    example: make stripe key optional

--- apps/example-saas/src/api/payments/index.ts
+++ apps/example-saas/src/api/payments/index.ts
@@ -6,4 +6,11 @@ import { StripePaymentService } from "./services/StripePaymentService.ts";
 export const SaasPayments = $module({
   name: "saas.api.payments",
   services: [StripePaymentService, PaymentController, StripeWebhookController],
+  register: (alepha) => {
+    if (alepha.env.STRIPE_SECRET_KEY) {
+      alepha.with(PaymentController);
+      alepha.with(StripePaymentService);
+      alepha.with(StripeWebhookController);
+    }
+  },
 });
