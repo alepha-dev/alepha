1b49758f4da89c7f9aba4bfd407da7ab44b5ec34
Nicolas Foures <ni.foures@gmail.com>
2026-01-07 12:04:26 +0100

    devtools: inline sqlite migrations

--- packages/devtools/src/api/providers/DevToolsDatabaseProvider.ts
+++ packages/devtools/src/api/providers/DevToolsDatabaseProvider.ts
@@ -8,4 +8,20 @@ export class DevToolsDatabaseProvider extends NodeSqliteProvider {
   protected readonly options = {
     path: ":memory:",
   };
+
+  protected override async migrateDatabase(): Promise<void> {
+    this.sqlite.exec(`
+        CREATE TABLE IF NOT EXISTS logs (
+          id INTEGER PRIMARY KEY AUTOINCREMENT,
+          level TEXT NOT NULL,
+          message TEXT,
+          service TEXT,
+          module TEXT,
+          context TEXT,
+          app TEXT,
+          data TEXT,
+          timestamp INTEGER NOT NULL
+        )
+      `);
+  }
 }
