216019983385c84e839b23586d6901938278caed
Nicolas Foures <ni.foures@gmail.com>
2026-04-23 12:17:42 +0200

    fix(project): fix bad path mapping

--- packages/alepha/src/cli/core/alephaPackageJson.ts
+++ packages/alepha/src/cli/core/alephaPackageJson.ts
@@ -1,4 +1,4 @@
-import pkg from "../../../package.json" with { type: "json" };
+import pkg from "alepha/package.json" with { type: "json" };
 
 export const alephaPackageJson = pkg;
 export const version = pkg.version as string;
